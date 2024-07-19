2bae7cad5eaa32512190741c11c0c101c3d8c118
havetodo-yeon <lotusdo216@gmail.com>
2024-07-19 17:46:59 +0900

    TBD-18 포탑 FSM 생성
    1. 포탑이 각 상태로 전환이 원활하게 하기 위한 FSM의 틀을 구현하였습니다.
    
    2. FSM 연결이 잘 되어있는지 파라미터를 수동으로 조작하여 확인하였습니다.
    
    3. 해당 FSM의 파라미터를 조작하여 Turret의 상태를 바꾸어줄 StateMachine 클래스를 구축하였습니다.
    
    4. Turret : 포탑의 HP등 전반적인 상황 조작
    TurretState : 각 State별 입출력 조건 및 행동 사항
    TurretStateMachine : 포탑의 StateMachine
    
    5. 그러나 아직 해당 스크립트가 해당 State에서 작동하는지 디버깅 하지 못했습니다. 이것은 TBD-40에서 구체화 할 내용입니다.
    
    6. TurretDetectionState클래스는 탐지 상태 동작에 대한 클래스로 이 또한 TBD-40에서 구체화 할 내용입니다.

---
+++ Assets/Scripts/Turret/Turret.cs
@@ -0,0 +1,36 @@
+﻿using Unity.VisualScripting;
+using UnityEngine;
+
+public class Turret : MonoBehaviour
+{
+    [SerializeField] public float turretHP;
+
+    #region States
+    public TurretStateMachine stateMachine { get; private set; }
+    public TurretDetectionState detectionState { get; private set; }
+    #endregion
+
+    #region Animator
+    public Animator anim { get; private set; }
+    #endregion
+
+    public void Awake()
+    {
+        stateMachine = new TurretStateMachine();
+        detectionState = new TurretDetectionState(stateMachine, this, "isIdle");
+    }
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+
+        stateMachine.Init(detectionState);
+    }
+
+    private void Update()
+    {
+        stateMachine.currentState.Update();
+    }
+
+    public void AnimationTrigger() => stateMachine.currentState.AnimationFinishTrigger();
+}
