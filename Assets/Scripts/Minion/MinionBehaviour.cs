8004ab03b2181609e2b49bfe910eff7c8211f830
gudals123 <104682516+gudals123@users.noreply.github.com>
2024-07-21 19:56:28 +0900

    미니언 FSM

---
+++ Assets/Scripts/Minion/MinionBehaviour.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinionBehaviour : MonoBehaviour
+{
+   public static readonly int hashInPursuit = Animator.StringToHash("InPursuit");
+   public static readonly int hashDetected = Animator.StringToHash("Detected");
+   public static readonly int hashAttack = Animator.StringToHash("Attack");
+   public static readonly int hashDie = Animator.StringToHash("Die");
+
+    public void TargetDetection()
+    {
+
+    }
+
+    public void TargetSelection()
+    {
+
+    }
+
+}
---
+++ Assets/Scripts/Minion/State/TrackState.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Animations;
+
+public class TrackState : StateMachineBehaviour
+{
+    private Animator _animator;
+
+    public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+
+    }
+
+    public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+
+    }
+
+    public override void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+
+    }
+}
