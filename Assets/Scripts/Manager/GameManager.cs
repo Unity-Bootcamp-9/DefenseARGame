4e231066d0018e8f6fbe87bcd1568473b69e696e
Canachii <choimoohun@naver.com>
2024-07-22 10:13:37 +0900

    TBD-20 코드 수정 및 필드 설명 추가

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -1,24 +1,40 @@
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.XR.Interaction.Toolkit.Interactables;
 using UnityEngine.XR.Interaction.Toolkit.Samples.StarterAssets;
 
 public class GameManager : MonoBehaviour
 {
-    [SerializeField] private GameObject mapPrefab;
+    [Tooltip("맵 미리보기 UI 프리팹")]
+    public GameObject mapPreviewPrefab;
+
+    [Tooltip("게임 시작 후 사용할 맵 프리팹")]
+    public GameObject mapPrefab;
+
+    [Tooltip("미리보기를 생성할 오브젝트 스포너")]
+    public ObjectSpawner objectSpawner;
+
+    private void Reset()
+    {
+        objectSpawner = FindObjectOfType<ObjectSpawner>();
+    }
+
+    private void Start()
+    {
+        objectSpawner.objectPrefabs = new List<GameObject>() { mapPreviewPrefab };
+    }
 
     public void StartGame()
     {
-        GameObject mapPreview = FindObjectOfType<XRGrabInteractable>().gameObject;
+        Transform mapPreview = objectSpawner.transform.GetChild(0);
 
         if (!mapPreview) return;
 
         if (mapPrefab)
         {
-            GameObject temp = Instantiate(mapPrefab, mapPreview.transform.position, mapPreview.transform.rotation);
-            temp.transform.localScale = mapPreview.transform.localScale * 1e-2f;
-            temp.transform.Rotate(Vector3.up, -45f);
-            Destroy(mapPreview);
+            GameObject map = Instantiate(mapPrefab, mapPreview.position, mapPreview.rotation);
+            map.transform.localScale = mapPreview.localScale * 1e-2f;
+            map.transform.Rotate(Vector3.up, -45f);
+            Destroy(objectSpawner);
         }
     }
 }
