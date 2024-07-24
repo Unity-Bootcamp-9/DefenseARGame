345e6efcafaaa8b2d01e7f3803c756799cbce5fa
Canachii <choimoohun@naver.com>
2024-07-24 14:49:01 +0900

    TBD-46 스킬 사용 불가 상태 및 흑백 셰이더 추가

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit.Samples.StarterAssets;
@@ -13,14 +14,84 @@ public class GameManager : MonoBehaviour
     [Tooltip("미리보기를 생성할 오브젝트 스포너")]
     public ObjectSpawner objectSpawner;
 
+    [Tooltip("게임 시작 여부")]
+    public bool isPlaying;
+
+    #region 스킬
+
+    public const int MaxMana = 10;
+    public static int CurrentMana { get; private set; }
+
+    private readonly float _manaRegenInterval = 2.0f;
+    private float _manaTimer;
+
+    [Tooltip("스킬 아이콘")]
+    public Skill[] skills = new Skill[4];
+
+    public event Action ManaChanged;
+
+    private void AddObserverOfMana()
+    {
+        foreach (var skill in skills)
+        {
+            ManaChanged += skill.ChangeColor;
+            skill.ChangeColor();
+        }
+
+        ManaChanged += () =>
+        {
+            Debug.Log($"Current Mana : {CurrentMana}/{MaxMana}");
+        };
+    }
+
+    private void RemoveObserverOfMana()
+    {
+        foreach (var skill in skills)
+        {
+            ManaChanged += skill.ChangeColor;
+            skill.ChangeColor();
+        }
+    }
+
+    private void AddMana()
+    {
+        _manaTimer += Time.deltaTime;
+
+        if (_manaTimer >= _manaRegenInterval && CurrentMana < MaxMana)
+        {
+            _manaTimer = 0;
+            CurrentMana++;
+            ManaChanged?.Invoke();
+        }
+    }
+
+    #endregion
+
     private void Reset()
     {
         objectSpawner = FindObjectOfType<ObjectSpawner>();
+        skills = FindObjectsOfType<Skill>();
     }
 
     private void Start()
     {
-        objectSpawner.objectPrefabs = new List<GameObject>() { mapPreviewPrefab };
+        if (objectSpawner) objectSpawner.objectPrefabs
+                = new List<GameObject>() { mapPreviewPrefab };
+
+        AddObserverOfMana();
+    }
+
+    private void OnDisable()
+    {
+        RemoveObserverOfMana();
+    }
+
+    private void Update()
+    {
+        if (isPlaying)
+        {
+            AddMana(); 
+        }
     }
 
     public void StartGame()
@@ -35,6 +106,8 @@ public class GameManager : MonoBehaviour
             map.transform.localScale = mapPreview.localScale * 1e-2f;
             map.transform.Rotate(Vector3.up, -45f);
             Destroy(objectSpawner);
+
+            isPlaying = true;
         }
     }
 }
