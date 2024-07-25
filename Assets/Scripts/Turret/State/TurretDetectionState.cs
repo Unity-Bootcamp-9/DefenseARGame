800b3acd8ec4ddae2ed2800d7b9cbba41ac71b79
gudals123 <wnsals2111@naver.com>
2024-07-25 09:57:55 +0900

    임시저장

--- Assets/Scripts/Turret/State/TurretDetectionState.cs
+++ Assets/Scripts/Turret/State/TurretDetectionState.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 public class TurretDetectionState : StateMachineBehaviour
 {
     TurretBehavior turretBehavior;
+    Projectile projectile;
 
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         turretBehavior = animator.GetComponent<TurretBehavior>();
+        projectile = animator.GetComponentInChildren<Projectile>();
+
     }
 
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
@@ -17,5 +20,6 @@ public class TurretDetectionState : StateMachineBehaviour
 
     override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
+        projectile.target = turretBehavior.target;
     }
 }
