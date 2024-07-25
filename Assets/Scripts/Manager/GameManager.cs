dc93ccb3e7bb42eb4d622e8313f66bc644ab066b
Canachii <42132716+Canachii@users.noreply.github.com>
2024-07-25 09:50:00 +0900

    TBD-74 잘못된 연산자 수정

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -49,7 +49,7 @@ public class GameManager : MonoBehaviour
     {
         foreach (var skill in skills)
         {
-            ManaChanged += skill.ChangeColor;
+            ManaChanged -= skill.ChangeColor;
             skill.ChangeColor();
         }
     }
