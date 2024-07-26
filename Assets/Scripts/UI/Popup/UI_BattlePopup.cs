4632d5f4d8858beac4c25ec1e3caca5cb9216ce1
havetodo-yeon <lotusdo216@gmail.com>
2024-07-26 16:14:14 +0900

    TBD-83 UI Popup Design
    공격, 결과창 팝업 버튼 연결을 완료하였습니다.

--- Assets/Scripts/UI/Popup/UI_BattlePopup.cs
+++ Assets/Scripts/UI/Popup/UI_BattlePopup.cs
@@ -4,34 +4,86 @@ using static Define;
 
 public class UI_BattlePopup : UI_Popup
 {
-/*    enum Buttons
+    enum Texts
     {
-        MapSettingButton,
+        PlayTimeText
     }
-*/
+
+    enum Buttons
+    {
+        Skill1Button,
+        Skill2Button,
+        Skill3Button,
+        Skill4Button,
+    }
+
+    enum States
+    {
+        Defeat,
+        Victory,
+        Default
+    }
+
     public override bool Init()
     {
         if (base.Init() == false)
             return false;
 
-/*        //BindText(typeof(Texts));
+        BindText(typeof(Texts));
+        BindImage(typeof(Buttons));
         BindButton(typeof(Buttons));
 
-        GetButton((int)Buttons.MapSettingButton).gameObject.BindEvent(OnClickMapSettingButton);
-
-*/
+        GetButton((int)Buttons.Skill1Button).gameObject.BindEvent(OnClickSkill1Button);
+        GetButton((int)Buttons.Skill2Button).gameObject.BindEvent(OnClickSkill2Button);
+        GetButton((int)Buttons.Skill3Button).gameObject.BindEvent(OnClickSkill3Button);
+        GetButton((int)Buttons.Skill4Button).gameObject.BindEvent(OnClickSkill4Button);
 
         return true;
     }
 
-/*    void OnClickMapSettingButton()
+    void OnClickSkill1Button()
     {
-        Debug.Log("OnClickMapSettingButton");
-        Managers.Game.StartGame();
-        Managers.UI.ClosePopupUI(this);
-
-        //Managers.UI.ShowPopupUI<UI_BattlePopup>();
-
+        Debug.Log("OnClickSkill1Button");
     }
-*/
+
+    void OnClickSkill2Button()
+    {
+        Debug.Log("OnClickSkill2Button");
+    }
+
+    void OnClickSkill3Button()
+    {
+        Debug.Log("OnClickSkill3Button");
+    }
+
+    void OnClickSkill4Button()
+    {
+        Debug.Log("OnClickSkill4Button");
+    }
+
+    States state = States.Default;
+    float time = 0;
+
+    private void Update()
+    {
+        time += Time.deltaTime;
+        if (time > 3)
+        {
+            state = States.Victory;
+        }
+        switch (state)
+        {
+            case States.Defeat:
+                Managers.UI.ClosePopupUI(this);
+                Managers.UI.ShowPopupUI<UI_ResultDefeatPopup>();
+                break;
+            case States.Victory:
+                Managers.UI.ClosePopupUI(this);
+                Managers.UI.ShowPopupUI<UI_ResultVictoryPopup>();
+                break;
+            default:
+                break;
+        }
+    }
+
 }
