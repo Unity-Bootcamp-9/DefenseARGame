cbadabf526c5dfb72396e74d0d9b25e9f98bd27d
havetodo-yeon <lotusdo216@gmail.com>
2024-07-31 17:51:58 +0900

    TBD-114-UI-ToolTip
    ## 설명
    - icon에 버튼 사용하면 안됨. Image로 구현.

--- Assets/Scripts/UI/Popup/UI_BattlePopup.cs
+++ Assets/Scripts/UI/Popup/UI_BattlePopup.cs
@@ -1,12 +1,26 @@
 using System;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 using static Define;
 
 public class UI_BattlePopup : UI_Popup
 {
+    // enum을 정의할 때, 0번째부터 3번째 까지는 반드시 Tooltip 관련 오브젝트 이름을 순서대로 정의해주세요.
+    enum GameObjects
+    {
+        ToolTip1,
+        ToolTip2,
+        ToolTip3,
+        ToolTip4,
+    }
+
     enum Texts
     {
+        ToolTipText1,
+        ToolTipText2,
+        ToolTipText3,
+        ToolTipText4,
         PlayTimeText,
     }
 
@@ -21,11 +35,16 @@ public class UI_BattlePopup : UI_Popup
 
     enum Buttons
     {
+        Skill1Image,
+        Skill2Image,
+        Skill3Image,
+        Skill4Image,
         PauseButton,
     }
 
     float sec;
     int min;
+    bool isOnClick;
 
     public override bool Init()
     {
@@ -35,21 +54,44 @@ public class UI_BattlePopup : UI_Popup
         BindText(typeof(Texts));
         BindImage(typeof(Images));
         BindButton(typeof(Buttons));
+        BindObject(typeof(GameObjects));
 
-        min = 0;
-        sec = 0;
+        RefreshUI();
+
+        return true;
+    }
+
+    private void RefreshUI()
+    {
+        GetImage((int)Images.Skill1Image).gameObject.GetOrAddComponent<MeteorSkill>();
+        GetImage((int)Images.Skill2Image).gameObject.GetOrAddComponent<MonsoonSkill>();
+        GetImage((int)Images.Skill3Image).gameObject.GetOrAddComponent<MonsoonSkill>(); // 임시
+        GetImage((int)Images.Skill4Image).gameObject.GetOrAddComponent<HealSkill>();
 
         GetText((int)Texts.PlayTimeText).text = "00:00";
 
-        GetImage((int)Images.Skill1Image).gameObject.GetOrAddComponent<MeteorSkill>();
-        GetImage((int)Images.Skill2Image).gameObject.GetOrAddComponent<MonsoonSkill>();
-        GetImage((int)Images.Skill3Image).gameObject.GetOrAddComponent<Skill>();
-        GetImage((int)Images.Skill4Image).gameObject.GetOrAddComponent<HealSkill>();
         GetImage((int)Images.Steminas).gameObject.GetOrAddComponent<Mana>().FindListener();
 
         GetButton((int)Buttons.PauseButton).gameObject.BindEvent(OnClickPauseButton);
 
-        return true;
+        SetTooltipInfo();
+
+        min = 0;
+        sec = 0;
+        isOnClick = false;
+    }
+
+    private void SetTooltipInfo()
+    {
+        Skill skill;
+        for(int i  = 0; i < 4; i++)
+        {
+            skill = GetImage(i).gameObject.GetOrAddComponent<Skill>();
+            GetText(i).text = $"스킬 이름 : {skill.SkillName_KR}\n스킬 설명 : {skill.Description}\n필요 마나 : {skill.RequireMana}\n피해량 : {skill.Damage}\n스킬 범위 : {skill.Radius}";
+            GetButton(i).gameObject.BindEvent(OnPressImage, Define.UIEvent.Pressed);
+            GetButton(i).gameObject.BindEvent(OnPointerUpImage, Define.UIEvent.PointerUp);
+            GetObject(i).SetActive(false);
+        }
     }
 
     private void Update()
@@ -71,4 +113,27 @@ public class UI_BattlePopup : UI_Popup
         Managers.UI.ShowPopupUI<UI_MapSettingPopup>();
         Managers.Game.PauseGame();
     }
+
+    float time = 0;
+
+    int index = -1;
+
+    void OnPressImage()
+    {
+        time = 0;
+        if (time > 1f)
+        {
+            GetObject(index).SetActive(true);
+        }
+        else
+        {
+            time += Time.deltaTime;
+        }
+    }
+    void OnPointerUpImage()
+    {
+        gameObject.SetActive(false);
+    }
+
+    // 손 떼면 온클릭 false로 바꾸고 툴팁 오프
 }
